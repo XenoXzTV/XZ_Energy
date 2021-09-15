6940cf8d19ebd0a085f1c6d395ffae14153918b6
XenoXzTV <90718666+XenoXzTV@users.noreply.github.com>
2021-09-15 23:37:15 +0200

    Update fxmanifest.lua

--- XZ_energy/fxmanifest.lua
+++ XZ_energy/fxmanifest.lua
@@ -7,7 +7,7 @@ version '1.0.0'
 client_scripts {
     '@es_extended/locale.lua',
     'locales/en.lua',
-	'locales/it.lua',
+    'locales/it.lua',
     'config.lua',
     'client/main.lua'
 }
@@ -15,7 +15,7 @@ client_scripts {
 server_scripts {
     '@es_extended/locale.lua',
     'locales/en.lua',
-	'locales/it.lua',
+    'locales/it.lua',
     'config.lua',
     'server/main.lua'
 }
